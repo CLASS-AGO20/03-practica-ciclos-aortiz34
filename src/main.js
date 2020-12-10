dd75d9a37b7a13405d678486c4c85b5d7f2a9d23
aortiz34 <aortiz34@ucol.mx>
2020-12-10 15:44:55 -0600

    Added the first function factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-export default class App {}
+export default class App {
+
+    factorial(numero){
+        let factorial = 1;
+        let i = 1;
+        for (i; i <= numero; i++) {
+            factorial *= i;  
+        }
+        return factorial;
+    }
+}
 
 let app = new App();
+
+console.log(app.factorial(5));
